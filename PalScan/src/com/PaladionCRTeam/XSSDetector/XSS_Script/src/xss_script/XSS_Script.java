9bf144f5f583fce161874459abfdac11789143c0
sumukh.r <sumukh.r@PAL-BLR-SUMU.PALBLR.NET>
2017-03-31 12:58:55 +0530

    updated

--- PalScan/src/com/PaladionCRTeam/XSSDetector/XSS_Script/src/xss_script/XSS_Script.java
+++ PalScan/src/com/PaladionCRTeam/XSSDetector/XSS_Script/src/xss_script/XSS_Script.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package xss_script;
+package com.PaladionCRTeam.XSSDetector.XSS_Script.src.xss_script;
 
 import java.io.BufferedReader;
 import java.io.File;
